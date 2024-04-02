28eed39abaa5e8a285be44e6e255b4c5c1ff6376
hmmini <pak4184@naver.com>
2024-04-02 17:44:38 +0900

    [Servlet]
    - HttpRequest, HttpResponse 에 대한 이해
    - Servlet 에 대한 이해

---
+++ spring-mvc01/src/main/java/com/spring/mvc/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,31 @@
+package com.spring.mvc.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.mvc.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+    }
+}
